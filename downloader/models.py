5bbaa4fde9562287ee82dd2be79ba1932eead360
azat715 <azat715@gmail.com>
2022-01-02 23:37:43 +0500

    init

---
+++ downloader/models.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, Field, HttpUrl
+
+
+class Album(BaseModel):
+    """album json сериализация
+
+    Example:
+        {
+            "userId": 1,
+            "id": 1,
+            "title": "quidem molestiae enim"
+        }
+    """
+    userId: int
+    id_: int =  Field(alias='id')
+    title: str
+
+
+class Photo(BaseModel):
+    """photo json сериализация
+
+    Example:
+        {
+            "albumId": 1,
+            "id": 1,
+            "title": "accusamus beatae ad facilis cum similique qui sunt",
+            "url": "https://via.placeholder.com/600/92c952",
+            "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+        }
+    """
+    albumId: int
+    id_: int =  Field(alias='id')
+    title: str
+    url: HttpUrl
+    thumbnailUrl: HttpUrl
---
+++ downloader/utils.py
@@ -0,0 +1,86 @@
+import functools
+import time
+from typing import Any, List, Mapping, Union, Type
+
+import requests
+from loguru import logger
+from pydantic import BaseModel, parse_obj_as
+
+
+class Get:
+    def get(self, url: str) -> requests.Response:
+        """скачивание
+
+        Returns:
+            [requests.Response]
+
+        Raises:
+            [HTTPError]: ошибка скачивания
+        """
+        r = requests.get(url)
+        r.raise_for_status()
+        return r
+
+    def get_json(self, url: str) -> List[dict]:
+        """скачивание json
+
+        Returns:
+            [list[dict]] список словарей json
+
+        """
+        return self.get(url).json()
+
+    def get_bytes(self, url: str) -> bytes:
+        return self.get(url).content
+
+
+class Serializer:
+    def __init__(self, model: Type[BaseModel]) -> None:
+        self.model = model
+
+    def serialize(self, data: Mapping, many=False) -> Any:
+        """сериализация
+
+        Args:
+            data ([dict]): словарь json
+            many ([bool]), default=False: работа со списком
+
+        Returns:
+            [list]: список Pydantic models
+
+        Raises:
+            [ValidationError]: ошибка валидации pydantic
+        """
+        if many:
+            return parse_obj_as(list[self.model], data)
+        else:
+            return self.model(**data)
+
+
+class Timer:
+    def __init__(self) -> None:
+        self.t_start = time.monotonic()
+
+    def stop(self) -> float:
+        return time.monotonic() - self.t_start
+
+    @classmethod
+    def start(cls) -> "Timer":
+        return cls()
+
+
+def logger_wraps(logger):
+    def wrapper(func):
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            logger.opt(depth=1)
+            logger.debug(f"Загрузка url {args[1]} началась")
+            t = Timer.start()
+            result = func(*args, **kwargs)
+            time_delta = t.stop()
+            logger.debug(f"Загрузка url {args[1]} закончилась за {time_delta}")
+            return result
+
+        return wrapped
+
+    return wrapper
